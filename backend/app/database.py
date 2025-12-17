5e7e14fbdeaf7cda1aaa77de3dfcaefa61a942d2
dong1203 <hambur1203@gmail.com>
2025-12-17 14:06:53 +0900

    데이터베이스 코드 최종 수정

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,27 +8,26 @@ load_dotenv()
 
 db_url = os.getenv("DATABASE_URL")
 
-# 데이터 베이스 연결(echo=True: SQL 쿼리 로깅 활성화, pool_pre_ping=True: 연결 유효성 검사)
-engine = create_engine(db_url, echo=True, pool_pre_ping=True)
+# 데이터베이스 엔진 생성
+engine = create_engine(
+    db_url,
+    echo=True,           # SQL 쿼리 로깅
+    pool_pre_ping=True   # 연결 유효성 검사
+)
+
+# 세션 팩토리 생성
+Session = sessionmaker(
+    autocommit=False,  # 트랜잭션 수동 관리
+    autoflush=False,   # 명시적 flush
+    bind=engine
+)
 
 # 베이스 클래스 생성
 Base = declarative_base()
 
-# 세션 팩토리 생성
-Session = sessionmaker(bind=engine)
-
-# 세션 객체 생성
-session = Session()
-
-# 세션을 통해 데이터베이스 작업 수행
-# 예: session.query(모델).filter(조건).all()
-
-# 세션 종료
-session.close()
-
 def get_db():
     """FASTAPI의 의존성 주입용 함수"""
-    db = SessionLocal()
+    db = Session()
 
     try:
         yield db
